61d46291ec3647809df5bc5f51171c21c6d4ae1e
Irine7 <irine7@yandex.ru>
2025-03-26 14:44:17 +0100

    feat: add translations

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,10 +1,13 @@
 import Link from 'next/link';
+import { useTranslations } from 'next-intl';
 
 const About: React.FC = () => {
+	const t = useTranslations('HomePage');
+
 	return (
 		<div>
-			<h1 className="text-3xl">About Page</h1>
-			<Link href="./">← Home Page</Link>
+			<h1 className="text-3xl">{t('about_page')}</h1>
+			<Link href="./">{t('go_to_home_page')}</Link>
 		</div>
 	);
 };
--- src/components/LayoutHeader.tsx
+++ src/components/LayoutHeader.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import React from 'react';
-import ThemeSwitcher from './ThemeSwitcher';
+import Link from 'next/link';
 import { useTranslations } from 'next-intl';
+
+import ThemeSwitcher from './ThemeSwitcher';
 import LanguageSwitcher from './language-switcher';
 
 function LayoutHeader() {
@@ -11,12 +13,14 @@ function LayoutHeader() {
 	return (
 		<header className="p-4 border-b">
 			<div className="container mx-auto flex justify-between items-center">
-				<h1 className="inline bg-gradient-to-r from-green-200 via-green-300 to-green-500 bg-clip-text text-3xl font-semibold leading-tight tracking-tight text-transparent lg:text-5xl">
-					{t('title')}
-				</h1>
+				<Link href="./">
+					<h1 className="inline bg-gradient-to-r from-green-200 via-green-300 to-green-500 bg-clip-text text-3xl font-semibold leading-tight tracking-tight text-transparent lg:text-5xl">
+						{t('title')}
+					</h1>
+				</Link>
 				<div className="flex gap-2 items-center">
 					<LanguageSwitcher />
-					<h3>{t('caldron')}</h3>
+					<h3>{t('cart')}</h3>
 					<ThemeSwitcher />
 				</div>
 			</div>
